96e0304ae72bcd7c7faebb925db8a67c81950cef
willfynch <william.finzy@protonmail.com>
2024-01-17 12:00:14 +0100

    Simplify layout / finish

--- src/app/components/Hero/Hero.tsx
+++ src/app/components/Hero/Hero.tsx
@@ -9,46 +9,47 @@ import Link from 'next/link'
 export default function Hero() {
 
     return (
+        <section id='SECTION_HERO' className='h-screen flex flex-col justify-center items-center '>
+            <div className={'  flex flex-col justify-center items-center px-10 md:px-20'}>
 
-        <section className={' h-full flex flex-col justify-center items-center mt-10 px-10 md:px-20 py-12'}>
-
-            <div className={'flex justify-center items-center gap-x-4 h-full'}>
-                <Image className={styles.circleImage + ` `} height={300} width={300} alt="avatar" src={avatar}></Image>
-                <div>
-                    <h1 id='HERO_TITLE' className=' text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-black'>Hello, I am William</h1>
-                    <h2 id='HERO_SUBTITLE' className=' text-xl sm:text-2xl md:text-3xl lg:text-4xl'>A web developer</h2>
+                <div className={'flex justify-center items-center gap-x-4 h-full'}>
+                    <Image className={styles.circleImage + ` `} height={300} width={300} alt="avatar" src={avatar}></Image>
+                    <div>
+                        <h1 id='HERO_TITLE' className='text-xl xs:text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-black'>Hello, I am William</h1>
+                        <h2 id='HERO_SUBTITLE' className=' text-xl sm:text-2xl md:text-3xl lg:text-4xl'>A web developer</h2>
+                    </div>
                 </div>
+
+
+                <p className='text-center mt-8 mb-4'>I design and build responsive and user-friendly websites and applications.</p>
+
+                <ul className='mt-8 flex flex-row justify-center'>
+                    <li className={styles.socialItemLi}>
+                        <FaLinkedin className='text-x3xl opacity-60 hover:opacity-100'></FaLinkedin>
+                    </li>
+                    <li className={styles.socialItemLi}>
+                        <FaSquareXTwitter className='text-x3xl opacity-60 hover:opacity-100'></FaSquareXTwitter>
+                    </li>
+                    <li className={styles.socialItemLi}>
+                        <FaInstagram className='text-x3xl opacity-60 hover:opacity-100'></FaInstagram>
+                    </li>
+                    <li className={styles.socialItemLi}>
+                        <FaGithubSquare className='text-x3xl opacity-60 hover:opacity-100'></FaGithubSquare>
+                    </li>
+                </ul>
+
+
+                <div className='mt-4 w-full md:w-1/2 text-center'>
+                    <button className={`${styles.btn} w-full sm:w-1/2`}>Get in touch !</button>
+                </div>
+
+
+                <div>
+
+                </div>
+
+
             </div>
-
-
-            <p>I design and build responsive and user-friendly websites and applications.</p>
-
-            <ul className='mt-8 flex flex-row justify-center'>
-                <li className={styles.socialItemLi}>
-                    <FaLinkedin className='text-x3xl opacity-60 hover:opacity-100'></FaLinkedin>
-                </li>
-                <li className={styles.socialItemLi}>
-                    <FaSquareXTwitter className='text-x3xl opacity-60 hover:opacity-100'></FaSquareXTwitter>
-                </li>
-                <li className={styles.socialItemLi}>
-                    <FaInstagram className='text-x3xl opacity-60 hover:opacity-100'></FaInstagram>
-                </li>
-                <li className={styles.socialItemLi}>
-                    <FaGithubSquare className='text-x3xl opacity-60 hover:opacity-100'></FaGithubSquare>
-                </li>
-            </ul>
-
-
-            <div className='mt-4 w-full md:w-1/2 text-center'>
-                <button className={`${styles.btn} w-full sm:w-1/2`}>Get in touch !</button>
-            </div>
-
-
-            <div>
-                
-            </div>
-
-
         </section>
 
     )
