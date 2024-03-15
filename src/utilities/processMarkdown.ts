57746301086ad2cd94c99fbff62c097ec80adeab
willfynch <william.finzy@protonmail.com>
2024-03-15 11:00:23 +0100

    refonte / wip

--- src/utilities/processMarkdown.ts
+++
@@ -1,8 +0,0 @@
-// import remark from 'remark';
-// import html from 'remark-html';
-
-// export const processMarkdown = async (markdown: string) => {
-//   const processor = remark().use(html);
-//   const result = await processor.process(markdown);
-//   return result.toString();
-// };
