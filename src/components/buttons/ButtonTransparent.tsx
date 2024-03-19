cb9e9545f79805e6bceb594ff49ccd50d7b88963
willfynch <william.finzy@protonmail.com>
2024-03-19 15:25:16 +0100

    refonte / wip

--- src/components/buttons/ButtonTransparent.tsx
+++ src/components/buttons/ButtonTransparent.tsx
@@ -1,12 +1,13 @@
 export interface ButtonTransparentProps {
     text: string;
     id: string;
+    width?:number;
     onClick?: ()=>void
 }
 
 export default function ButtonTransparent(props: ButtonTransparentProps){
     return(
-        <button onClick={props.onClick} className=" text-normal border-2 border-my-white p-3 text-my-white rounded-xl hover:bg-my-white hover:text-my-black">
+        <button onClick={props.onClick} className={(props.width?`w-[${props.width}px]` : ' ')+" text-normal border-2 border-my-white p-3 text-my-white rounded-xl hover:bg-my-white hover:text-my-black"}>
             {props.text}
         </button>
     )
