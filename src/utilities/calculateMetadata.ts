cfbf5b53b8330a2497e887ecc3b205d02212ff7a
willfynch <william.finzy@protonmail.com>
2024-03-27 15:16:06 +0100

    update metadata

--- src/utilities/calculateMetadata.ts
+++ src/utilities/calculateMetadata.ts
@@ -7,8 +7,8 @@ export function calculateMetadata(
 
     return {
         title: title,
-        description: `Je m’appelle Will, j’aime le café, la poésie et je fabrique des sites web pour aventuriers modernes. Mais qu’est-ce qu’un aventurier moderne, me diras-tu, toi qui me lis ?`,
-        keywords: ['Café', 'Développeur', 'Créateur de sites', 'Foire aux questions', 'Combien coûte un site web', 'Comment créer un site web', 'Développeur Normandie', 'Développeur Web', 'Développeur Rouen', 'Développeur', 'Site internet', 'Site vitrine', 'Site', 'Site e-commerce', 'E-commerce', 'TPE', 'Entrepreneurs', 'Sites jamstack', 'Sites sans serveur', 'Pas wordpress', 'Wordpress', 'Site sans wordpress', 'Site pas cher', 'Site économique'],
+        description: `Conception et réalisation de sites web sur mesure pour entrepreneuses, entrepreneurs, TPE et PME.`,
+        keywords: ['Développeur', 'Créateur de sites', 'Du café et du code', 'Combien coûte un site web', 'Création site web', 'Comment créer un site web', 'Développeur Normandie', 'Développeur Web', 'Développeur Rouen', 'Développeur', 'Site internet', 'Site vitrine', 'Site', 'Site e-commerce', 'E-commerce', 'TPE', 'Entrepreneurs', 'Sites jamstack', 'Sites sans serveur', 'Site sans wordpress', 'Conception de site web'],
         metadataBase: new URL('https://ducafeetducode.com'),
         alternates: {
           canonical: 'https://ducafeetducode.com' + canonical
@@ -19,7 +19,7 @@ export function calculateMetadata(
         twitter: {
           card: 'summary_large_image',
           images: ['/images/opengraph.png'],
-          description: `Je m’appelle Will, j’aime le café, la poésie et je fabrique des sites web pour aventuriers modernes. Mais qu’est-ce qu’un aventurier moderne, me diras-tu, toi qui me lis ?`,
+          description: `Conception et réalisation de sites web sur mesure pour aventuriers modernes.`,
           title: title
         },
         icons: [{ rel: 'icon', url: './favicon.ico' }, { rel: 'apple-touch-icon', url: './apple-touch-icon.png' }]
