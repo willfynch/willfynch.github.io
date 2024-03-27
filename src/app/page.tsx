750b511be253901de8f06d6b7d15b5e4a1834669
willfynch <william.finzy@protonmail.com>
2024-03-27 15:23:21 +0100

    update metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,10 @@
+import { calculateMetadata } from '@/utilities/calculateMetadata';
 import Hero from '../components/Hero/Hero'
 import { Metadata } from 'next'
 import { Bilbo_Swash_Caps } from 'next/font/google';
 import nowFont from 'next/font/local';
 
-export const metadata: Metadata = {
-  title: "Du café et du code | développement de sites web sur mesure",
-  description: `Sites vitrine et éditoriaux pour aventuriers modernes.`,
-  keywords: ['Café', 'Développeur', 'Créateur de sites', 'Développeur Normandie', 'Développeur Web', 'Développeur Rouen', 'Développeur', 'Site internet', 'Site vitrine', 'Site', 'Site e-commerce', 'E-commerce', 'TPE', 'Entrepreneurs', 'Sites jamstack', 'Sites sans serveur', 'Pas wordpress', 'Wordpress', 'Site sans wordpress', 'Site pas cher', 'Site économique'],
-  metadataBase: new URL('https://ducafeetducode.com'),
-  alternates: {
-    canonical: '/'
-  },
-  openGraph: {
-    images: ['/images/opengraph.png']
-  },
-  twitter: {
-    card: 'summary_large_image',
-    images: ['/images/opengraph.png'],
-    description: `Sites vitrine et éditoriaux pour aventuriers modernes.`,
-    title : 'Du café et du code | développement de sites web sur mesure'
-  },
-  icons: [{rel: 'icon', url:'./favicon.ico'}, {rel: 'apple-touch-icon', url:'./apple-touch-icon.png'}]
-};
+export const metadata: Metadata = calculateMetadata("Accueil", '/')
 
 const now = nowFont({
   src: '../../public/fonts/Now-Regular.otf'
