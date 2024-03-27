055bce441969a9ddd9d9417f1b2dc1495153d156
willfynch <william.finzy@protonmail.com>
2024-03-27 15:23:43 +0100

    update metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,8 @@
 import { calculateMetadata } from '@/utilities/calculateMetadata';
 import Hero from '../components/Hero/Hero'
 import { Metadata } from 'next'
-import { Bilbo_Swash_Caps } from 'next/font/google';
-import nowFont from 'next/font/local';
 
 export const metadata: Metadata = calculateMetadata("Accueil", '/')
-
-const now = nowFont({
-  src: '../../public/fonts/Now-Regular.otf'
-})
-
-const bilbo = Bilbo_Swash_Caps({
-  weight: '400',
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-bilbo',
-})
 
 export default function Home() {
   return (
