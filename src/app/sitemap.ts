f3aea088e5c6f032a813ee2f78bdfda280157afb
willfynch <william.finzy@protonmail.com>
2024-03-28 10:11:48 +0100

    sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -9,7 +9,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
-      url: 'https://ducafeetducode.com/about',
+      url: 'https://ducafeetducode.com/a-propos',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
