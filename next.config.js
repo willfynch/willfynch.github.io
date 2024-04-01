c43ac0bbdfc170df212af3601b0ee88d447c4dfc
willfynch <william.finzy@protonmail.com>
2024-04-01 11:24:49 +0200

    Add assets tina to next image domain

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // output: 'export',
-  // images: {
-  //   unoptimized: true,
-  // },
+  images: {
+    domains: ['assets.tina.io'],
+  },
   webpack: 
     (config) => {
       config.module.rules.push({
